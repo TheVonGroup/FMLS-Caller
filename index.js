40451098bd9769d24af41e55df09abf28dd17c95
Badar-BlueBird <abdulrehmanbadar191@gmail.com>
2024-03-29 23:13:50 +0500

    first

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello")
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 4000;
+
+// Define a route to make the Axios request
+app.get('/', async (req, res) => {
+  try {
+    // Make Axios request
+    console.log("Inside GET")
+    const response = await axios.get('https://fmls-scrapper.onrender.com');
+    console.log("After Axios GET")
+    // Send response back to client
+    res.json(response.data);
+  } catch (error) {
+    // Handle errors
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
