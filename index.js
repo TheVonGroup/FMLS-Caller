4a78e4c6ddfa7f3b01c64ebf093e914293167848
Badar-BlueBird <abdulrehmanbadar191@gmail.com>
2024-04-11 06:31:31 +0500

    commit 1

--- index.js
+++ index.js
@@ -7,15 +7,10 @@ const port = 4000;
 // Define a route to make the Axios request
 app.get('/', async (req, res) => {
   try {
-    // Make Axios request
-    console.log("Inside GET")
-    const response = await axios.get('https://fmls-scrapper.onrender.com');
-    console.log("After Axios GET")
-    // Send response back to client
+    const response = await axios.get('https://fmls-scrapper-stra.onrender.com');
     res.json(response.data);
   } catch (error) {
-    // Handle errors
-    console.error('Error fetching data:', error);
+    console.error('Error in making GET request:', error);
     res.status(500).json({ error: 'An error occurred while fetching data' });
   }
 });
